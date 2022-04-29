b920e585220868614dab4e76c84fc52fcb4946af
Eveline Debora Nuta <eveline.nuta@makingwaves.com>
2022-04-29 11:28:24 +0200

    create react proj

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ContactForm } from "./contactform";
+import { NewForm} from "./newform";
+
+function App() {
+  return (
+    <div>
+    <ContactForm />
+
+    <div>Next form</div>
+
+    <NewForm />
+    </div>
+  );
+}
+export default App;
